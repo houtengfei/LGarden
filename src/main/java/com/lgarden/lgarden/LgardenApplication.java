c83d2c18d5310182620d1bb1ae2582c4e716b709
tengfei <1025223755@qq.com>
2020-08-08 19:35:33 +0800

    git-learningGarden

---
+++ src/main/java/com/lgarden/lgarden/LgardenApplication.java
@@ -0,0 +1,13 @@
+package com.lgarden.lgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgardenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LgardenApplication.class, args);
+    }
+
+}
